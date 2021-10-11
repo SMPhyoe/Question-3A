2eebb6cc7b9ebfdce8904b4af934480f11ce7c54
SMPhyoe <sumyatphyoe.qqsm@gmail.com>
2021-10-11 16:08:06 +0800

    Done

---
+++ ConsumerAPI/Controllers/ConsumerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConsumerController : Controller
+    {
+        
+        [HttpGet("consume/all")]
+        public ActionResult<Message> GetAll()
+        {
+            Message msg = new Message();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    msg.messages = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", msg.messages);
+                    Trace.WriteLine(" [x] Received {0}", msg.messages);
+                };
+                channel.BasicConsume(queue: "TaskQueue",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+            return msg;
+
+        }
+    }
+}
---
+++ ConsumerAPI/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsumerAPI
+{
+    public class Message
+    {
+        public int ID { get; set; }
+        public string messages { get; set; }
+    }
+}
